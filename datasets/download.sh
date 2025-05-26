e0e215f2009859f95c531e1c100ad88e871edee3
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-05-26 11:08:48 +0100

    Provide data downloader

---
+++ datasets/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -L "https://www.dropbox.com/scl/fi/dfs6hs02arab7owdffnoa/MedClinSum.zip?rlkey=fdr8fdjterrygi265d5avw8r6&st=15350uet&dl=1" -o "archive.zip"
+unzip "archive.zip"
+rm "archive.zip"

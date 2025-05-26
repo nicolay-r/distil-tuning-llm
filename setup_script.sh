4b74f3abea3fdf5bf152ac820a15f9adfd17d8a4
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-05-26 10:50:10 +0100

    Removed non-utilized instructions

--- setup_script.sh
+++ setup_script.sh
@@ -16,13 +16,6 @@ sh cuda_12.0.0_525.60.13_linux.run
 # Navigate to the distill-d2n directory
 cd root/distill-d2n
 
-# Create an empty file named setup-get.sh in the current directory
-touch setup-git.sh
-
-# Create a directory named app_keys and an empty file named k.txt inside it
-mkdir -p app_keys
-touch app_keys/k.txt
-
 # create conda environments for fine-tuning
 conda env create -f environment.yml
 

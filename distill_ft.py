870032a11a877ae520a27ab649fbdbc5b1a3abcf
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-05-28 16:59:03 +0100

    Removed wrapper WIP.

--- distill_ft.py
+++ distill_ft.py
@@ -9,17 +9,9 @@ SUMMARIZE_PROMPT = 'Summarize: '
 EXTRACT_PROMPT = 'Extract: '
 
 
+# TODO. This function should support the mapping!
 def tokenizer_func(tokenizer, examples, features, max_length):
-    return tokenizer(*[examples[f] for f in features],
-                     max_length=max_length,
-                     padding="max_length",
-                     truncation=True)
-
-
-def wrapping_func(examples, prompt):
-    return {
-        'input': [" ".join(prompt, text) for text in examples]
-    }
+    return tokenizer(*[examples[f] for f in features], max_length=max_length, padding="max_length", truncation=True)
 
 
 def run(args):
@@ -30,6 +22,8 @@ def run(args):
 
     datasets = dataset_loader.load_from_json()
 
+    features_to_tokenize = ["input", "output"]
+
     if args.model_type == "standard":
 
         datasets = datasets.remove_columns("rationale")
@@ -38,14 +32,13 @@ def run(args):
             "input": SUMMARIZE_PROMPT + item["input"],
         }
 
-        features_to_tokenize = ["input", "output"]
-
     else:
         m_func = lambda item: {
             "input": SUMMARIZE_PROMPT + item["input"],
             "expl_input": EXTRACT_PROMPT + item["input"]
         }
-        features_to_tokenize = ["input", "output", "expl_input", "rationale"]
+
+        features_to_tokenize += ["expl_input", "rationale"]
 
     datasets = datasets.map(m_func)
 
@@ -55,7 +48,10 @@ def run(args):
         remove_columns=features_to_tokenize
     )
 
+    print(features_to_tokenize)
+    print(datasets)
     print(tokenized_datasets["train"][:5])
+    exit(0)
 
     train_and_evaluate(args=args, run=args.run, tokenizer=tokenizer,
                        tokenized_datasets=tokenized_datasets,
@@ -82,7 +78,7 @@ if __name__ == '__main__':
     parser.add_argument('--local_rank', type=int, default=-1)
     parser.add_argument('--gen_max_len', type=int, default=64)
     parser.add_argument('--parallelize', action='store_true')
-    parser.add_argument('--model_type', type=str, default='task_prefix')
+    parser.add_argument('--model_type', type=str, default='task_prefix', choices=["standard", "task_prefix"])
     parser.add_argument('--bf16', action='store_true')
     parser.add_argument('--no_log', action='store_true')
     parser.add_argument('--output_rationale', action='store_true')

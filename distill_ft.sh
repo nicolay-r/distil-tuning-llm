42b510bd0b1326c8f45ee259e7ae064bc99c17ee
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-05-29 11:04:01 +0100

    Switch to qwen

--- distill_ft.sh
+++ distill_ft.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 python distill_ft.py \
-    --from_pretrained "google/flan-t5-small" \
+    --from_pretrained "Qwen/Qwen2.5-0.5B-Instruct" \
     --dataset "multiclinsum_en_test" \
-    --eval_steps 10 \
-    --batch_size_train 1 \
-    --batch_size_eval 32 \
+    --eval_steps 50 \
+    --max_input_length 512 \
+    --batch_size_train 2 \
+    --batch_size_eval 2 \
     --grad_steps 1 \
     --weight 1 \
     --alpha 0.8 \
-    --model_type "task_prefix" \
+    --model_type "standard" \
     --addi_info "Additional-Notes" \
     --parallelize \
     --train_epochs 10 \
     --max_steps 36000 \
-    # --bf16 \
+    --bf16

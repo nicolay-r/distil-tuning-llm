73c62e61cfa40b3687d5c2f31c7460dd05c8d399
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-05-31 13:36:40 +0100

    Files relocation

--- datasets/make_dataset_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p "multiclinsum_en_test"
-cp multiclinsum/multiclinsum_gs_en.json "multiclinsum_en_test/train.json"
-cp multiclinsum/multiclinsum_gs_en.json "multiclinsum_en_test/valid.json"

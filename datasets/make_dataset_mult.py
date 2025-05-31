735e82c677e65720f8e319b09c1946c0d329d545
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-05-31 13:36:31 +0100

    Added dataset cropping

--- datasets/make_dataset_mult.py
+++ datasets/make_dataset_mult.py
@@ -1,19 +1,21 @@
 import json
 import os
 from os.path import join, dirname, realpath
-
 from utils import split_dataset
 
 
-# List of input JSON files
+#############################################
+# Initial parameters for setting up datasets.
+#############################################
+dataset_name = "multiclinsum"
 input_files = [
     "multiclinsum_gs_en.json",
     "multiclinsum_gs_es.json",
     "multiclinsum_gs_fr.json",
     "multiclinsum_gs_pt.json"
 ]
-
-# Split ratios
+input_max_length = 2560
+output_max_length = 512
 train_ratio = 0.8
 # We make validation ratio too small due to unknown memory leakage on resource consumption during evaluation.
 valid_ratio = 0.01
@@ -26,10 +28,10 @@ test_data = []
 # Loop over each file and perform the split
 current_dir = dirname(realpath(__file__))
 
-for file in input_files:
+for filename in input_files:
 
     train, valid, test = split_dataset(
-        json_path=join(current_dir, "multiclinsum", file),
+        json_path=join(current_dir, dataset_name, filename),
         train_ratio=train_ratio,
         valid_ratio=valid_ratio,
         test_ratio=test_ratio
@@ -38,6 +40,13 @@ for file in input_files:
     train_data += train
     valid_data += valid
     test_data += test
+
+
+for data in [train_data, valid_data, test_data]:
+    for item in data:
+        item["input"] = item["input"][:input_max_length]
+        item["output"] = item["output"][:output_max_length]
+        item["rationale"] = item["rationale"][:output_max_length]
 
 
 output_dir = "multiclinsum_mult"

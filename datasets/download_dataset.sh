316c3e81e1496befb1dcecf01e216ce4ec7cb4f5
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-06-02 09:36:15 +0100

    added annotated dataset

--- datasets/download_dataset.sh
+++ datasets/download_dataset.sh
@@ -2,3 +2,7 @@
 curl -L "https://www.dropbox.com/scl/fi/dfs6hs02arab7owdffnoa/MedClinSum.zip?rlkey=fdr8fdjterrygi265d5avw8r6&st=15350uet&dl=1" -o "archive.zip"
 unzip "archive.zip"
 rm "archive.zip"
+
+curl -L "https://www.dropbox.com/scl/fi/8bkjdlgh5w51p0kqtur66/MultiClinSum_rationale.zip?rlkey=qtsjwexzigshfyds7nkn1pod0&st=rw68uwcf&dl=1" -o "archive.zip"
+unzip "archive.zip"
+rm "archive.zip"

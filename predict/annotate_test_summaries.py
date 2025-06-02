258eed954244cffdf024cccb6c3f0d2f05d7ad8c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-06-02 20:26:04 +0100

    update

--- predict/annotate_test_summaries.py
+++ predict/annotate_test_summaries.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 from tqdm import tqdm
@@ -13,51 +14,65 @@ from cfg import DATASET_DIR, SUMMARIZE_PROMPT_LOCALE
 from keys import HF_API_KEY
 
 
-team_name = "bu_team"
+def run(args):
 
-submissions = {
-    1: "Qwen/Qwen2.5-0.5B-Instruct",
-    2: "nicolay-r/qwen25-05b-multiclinsum-standard",
-    3: "nicolay-r/qwen25-05b-multiclinsum-distil"
-}
+    team_name = "bu_team"
 
-subtasks = [
-   "multiclinsum_test_en",
-   "multiclinsum_test_es",
-   "multiclinsum_test_fr",
-   "multiclinsum_test_pt",
-]
+    submissions = {
+        1: "Qwen/Qwen2.5-0.5B-Instruct",
+        2: "nicolay-r/qwen25-05b-multiclinsum-standard",
+        3: "nicolay-r/qwen25-05b-multiclinsum-distil"
+    }
 
-for run_id in submissions.items():
+    subtasks = [
+       "multiclinsum_test_en",
+       "multiclinsum_test_es",
+       "multiclinsum_test_fr",
+       "multiclinsum_test_pt",
+    ]
 
-    for dataset_name in subtasks:
+    for run_id in submissions.items():
 
-        lang = dataset_name.split('_')[-1]
+        for dataset_name in subtasks:
 
-        input_dicts = list(map(
-            lambda r: {"filepath": r[0], "text": r[1]},
-            iter_text_files(folder_path=join(DATASET_DIR, dataset_name))
-        ))
+            lang = dataset_name.split('_')[-1]
 
-        content_it = iter_content(
-            schema={"schema": [{"prompt": SUMMARIZE_PROMPT_LOCALE[lang] + ": {text}", "out": "summary"}]},
-            llm=dynamic_init(class_filepath="providers/huggingface_qwen.py", class_name="Qwen2")(
-                api_token=HF_API_KEY,
-                model_name=submissions[run_id],
-                temp=0.1,
-                max_new_tokens=1024,
-                device='cuda'
-            ),
-            infer_mode="batch",
-            batch_size=10,
-            return_mode="record",
-            input_dicts_it=input_dicts,
-        )
+            input_dicts = list(map(
+                lambda r: {"filepath": r[0], "text": r[1]},
+                iter_text_files(folder_path=join(DATASET_DIR, dataset_name))
+            ))
 
-        write_text_files(
-            file_iter=map(
-                lambda r: (basename(r["filepath"]).split('.')[-2] + "_sum.txt", r["summary"]),
-                tqdm(content_it, desc=dataset_name, total=len(input_dicts))
-            ),
-            folder_path=join("submissions", f"{team_name}_multiclinsum_{lang}_run_{run_id}")
-        )
+            content_it = iter_content(
+                schema={"schema": [{"prompt": SUMMARIZE_PROMPT_LOCALE[lang] + ": {text}", "out": "summary"}]},
+                llm=dynamic_init(class_filepath="providers/huggingface_qwen.py", class_name="Qwen2")(
+                    api_token=HF_API_KEY,
+                    model_name=submissions[run_id],
+                    temp=0.1,
+                    max_new_tokens=1024,
+                    device=args.device
+                ),
+                infer_mode="batch",
+                batch_size=args.batch_size,
+                return_mode="record",
+                input_dicts_it=input_dicts,
+            )
+
+            write_text_files(
+                file_iter=map(
+                    lambda r: (basename(r["filepath"]).split('.')[-2] + "_sum.txt", r["summary"]),
+                    tqdm(content_it, desc=dataset_name, total=len(input_dicts))
+                ),
+                folder_path=join(args.output_dir, "submissions", f"{team_name}_multiclinsum_{lang}_run_{run_id}")
+            )
+        
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--device', type=str, default="cuda")
+    parser.add_argument('--output_dir', type=str, default=".")
+
+    args = parser.parse_args()
+
+    run(args)

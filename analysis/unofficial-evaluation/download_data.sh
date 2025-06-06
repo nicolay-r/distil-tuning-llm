1e5fb6dcd9200728539d4c0e1a40f5c1ad2e2211
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-06-06 11:04:34 +0100

    Added unofficial evaluation data

---
+++ analysis/unofficial-evaluation/download_data.sh
@@ -0,0 +1,3 @@
+curl -L "https://www.dropbox.com/scl/fi/2uhlgmuc1tk3rrsyytbo7/unofficial-evaluation.zip?rlkey=1k9r48ijtmaram1ix3q6vutd3&st=zyszh77u&dl=1" -o "evaluation.zip"
+unzip "evaluation.zip"
+rm -rf "evaluation.zip"
